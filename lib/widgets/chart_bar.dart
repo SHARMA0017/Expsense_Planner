c2b5ad35986feac1097e5eb49a4c92f006dead78
Sharma17 <bandhus152@gmail.com>
2022-01-04 15:38:36 +0530

    change

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors_in_immutables, prefer_const_constructors
-
+//Chat_Bar
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
